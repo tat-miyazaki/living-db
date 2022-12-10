37b73f1044cf1e88a787cae1aa117b7116da18cc
tat-miyazaki <75656722+tat-miyazaki@users.noreply.github.com>
2022-12-10 19:57:47 +0900

    Update and rename 1_createdb.sql to create.sql

---
+++ create.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE IF NOT EXISTS living;
+
+USE living;
+
+create table persons (
+  id int auto_increment primary key,
+  name text,
+  team text,
+  sex text,
+  nickname1 text,
+  nickname2 text,
+  profile text,
+  spell text,
+  extension1 text,
+  extension2 text,
+  imageurl text,
+  fixed int
+);
+
